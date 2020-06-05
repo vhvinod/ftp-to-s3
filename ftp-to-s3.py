418fdd47515ca4b390d97e8ab493d2a10eaf5ad0
Vinod Kumar V H <vhvinod@users.noreply.github.com>
2020-06-05 13:20:15 +0530

    Download the file from ftp and upload to s3

--- ftp-to-s3.py
+++ ftp-to-s3.py
@@ -1,3 +1,22 @@
-import json
+from ftplib import FTP
+import s3fs
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
+    s3 = s3fs.S3FileSystem(anon=False)
+	
+    ftp_url = "local"          #provide FTP host
+    ftp_path = "/test_folder/" #provide FTP path
+    s3Bucket = "s3-bucket"     #provide s3 bucket name
+    file_name = "sample.txt"   #provide file name
+
+    with FTP(ftp_url) as ftp:
+        ftp.login()
+        logger.info('Login Successful')
+        ftp.cwd(ftp_path)
+        logger.info('Downloading file: ' +file_name)
+        ftp.retrbinary('RETR ' + file_name, s3.open("{}/{}".format(s3Bucket, 'Inbound/'+file_name), 'wb').write)
+        logger.info('Download completed ' +file_name)
